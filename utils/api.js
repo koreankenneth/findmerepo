79e71b7413b63b1f9a32caecbcb3176b3cdb9824
Kenneth Kim <kenneth@Kennethui-MacBookPro.local>
2019-06-15 18:14:11 +0900

    commit for nothing

--- utils/api.js
+++ utils/api.js
@@ -8,7 +8,7 @@ const dummyData = {
     title: 'plese check this out!',
     author: 'nelo13',
     timestamp: '2019-03-04',
-    text: 'Please, find this item asap. I need to prepare it before my anniversay day.',
+    text: 'Please, find this item asap. I need to prepare it before the anniversay.',
     isSecret: true,
     item: {
       name: '18SS 골든구스 슈퍼스타 화이트',
